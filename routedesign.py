9822948b007ca93871b3184c0020f64d59c17049
苗祎迪 <miaoyidi@yidimiaosmini2.wifi.local.cmu.edu>
2024-04-01 18:39:46 -0400

    modified the plot labels etc to make it easier to understand, replot some graphs

--- routedesign.py
+++ routedesign.py
@@ -51,29 +51,29 @@ def plot_routes(X, Y, locations, route, tsp_dis, wlk_dis=0):
 
 
 
-size, magnitude = 50, 100
+num_demand, magnitude = 50, 100
 num_fixed_stops = 15
-discount_ratio_array = np.arange(0.1, 1, 0.1)
-sizes_array = np.array([25, 50, 75, 100, 150, 200, 300])
+discount_ratio_array = np.arange(0.1, 1, 0.2)
+num_demand_array = np.array([25, 50, 75, 100, 150, 200, 250, 300])
 # num_fixed_stops_array = np.around(discount_ratio * sizes_array)
 # num_fixed_stops_array = np.array([5, 15, 30, 50, 80, 100])
 
 
-for size in sizes_array:
+for num_demand in num_demand_array:
     ondmd_total_dises = []
     fx_total_dises, wlk_dises, tsp_dises = [], [], []
     num_stops = []
-    X = magnitude*np.random.rand(size)
-    Y = magnitude*np.random.rand(size)
+    X = magnitude*np.random.rand(num_demand)
+    Y = magnitude*np.random.rand(num_demand)
     locations = np.stack((X, Y), axis=1)
     distance_matrix = euclidean_distance_matrix(locations)
     route, dis = solve_tsp_simulated_annealing(distance_matrix)
 
     # for num_fixed_stops in num_fixed_stops_array:
     for discount_ratio in discount_ratio_array:
-        num_fixed_stops = int(np.around(size * discount_ratio))
+        num_fixed_stops = int(np.around(num_demand * discount_ratio))
         num_stops.append(num_fixed_stops)
-        if size > num_fixed_stops:
+        if num_demand > num_fixed_stops:
             # ax1 = plot_routes(X, Y, locations, route, dis)
             # ax1.scatter(X, Y)
 
@@ -83,8 +83,8 @@ for size in sizes_array:
             labels, centers = kmeans.labels_, kmeans.cluster_centers_
             center_X, center_Y = centers[:, 0], centers[:, 1]
             center_distance_matrix = euclidean_distance_matrix(centers)
-            # center_route, center_dis = solve_tsp_simulated_annealing(center_distance_matrix)
-            center_route, center_dis = solve_tsp_local_search(center_distance_matrix)
+            center_route, center_dis = solve_tsp_simulated_annealing(center_distance_matrix)
+            # center_route, center_dis = solve_tsp_local_search(center_distance_matrix)
             center_ctgr = np.array([centers[i] for i in labels])
             wlk_dis = np.sum(np.linalg.norm(locations - center_ctgr, axis=1))
             # ax2 = plot_routes(center_X, center_Y, centers, center_route, center_dis, wlk_dis)
@@ -102,23 +102,24 @@ for size in sizes_array:
     ax1.plot(num_stops[:len(tsp_dises)], fx_total_dises, label="Fixed route total distance")
     ax1.plot(num_stops[:len(tsp_dises)], wlk_dises, label="Fixed route walk distance")
     ax1.plot(num_stops[:len(tsp_dises)], tsp_dises, label="Fixed route tsp distance")
-    plt.xlabel('discount ratio')
+    plt.xlabel('Num stops')
     plt.ylabel('Distances')
     ax1.legend()
-    ax1.set_title(f'size {size} \'s distances')
-    fig1.savefig(f'variant_num_stops/demand_size_{size}', format='png')
+    ax1.set_title(f'number of demands {num_demand} \'s distances')
+    fig1.savefig(f'variant_num_stops/demand_size_{num_demand}.png')
 
 
 # for num_fixed_stops in num_fixed_stops_array:
+max_num_demand = num_demand_array[-1]
 for discount_ratio in discount_ratio_array:
-    num_fixed_stops = int(np.around(size * discount_ratio))
+    num_fixed_stops = int(np.around(max_num_demand * discount_ratio))
     ondmd_total_dises = []
     fx_total_dises, wlk_dises, tsp_dises = [], [], []
 
-    for size in sizes_array:
-        if size > num_fixed_stops:
-            X = magnitude*np.random.rand(size)
-            Y = magnitude*np.random.rand(size)
+    for num_demand in num_demand_array:
+        if num_demand > num_fixed_stops:
+            X = magnitude*np.random.rand(num_demand)
+            Y = magnitude*np.random.rand(num_demand)
             locations = np.stack((X, Y), axis=1)
             distance_matrix = euclidean_distance_matrix(locations)
             route, dis = solve_tsp_simulated_annealing(distance_matrix)
@@ -140,12 +141,12 @@ for discount_ratio in discount_ratio_array:
 
     fig2 = plt.figure()
     ax2 = fig2.add_subplot()
-    ax2.plot(sizes_array[len(sizes_array) - len(tsp_dises):], ondmd_total_dises, label="On demand total distance")
-    ax2.plot(sizes_array[len(sizes_array) - len(tsp_dises):], fx_total_dises, label="Fixed route total distance")
-    ax2.plot(sizes_array[len(sizes_array) - len(tsp_dises):], wlk_dises, label="Fixed route walk distance")
-    ax2.plot(sizes_array[len(sizes_array) - len(tsp_dises):], tsp_dises, label="Fixed route tsp distance")
-    plt.xlabel('Num of sizes of demands')
+    ax2.plot(num_demand_array[len(num_demand_array) - len(tsp_dises):], ondmd_total_dises, label="On demand total distance")
+    ax2.plot(num_demand_array[len(num_demand_array) - len(tsp_dises):], fx_total_dises, label="Fixed route total distance")
+    ax2.plot(num_demand_array[len(num_demand_array) - len(tsp_dises):], wlk_dises, label="Fixed route walk distance")
+    ax2.plot(num_demand_array[len(num_demand_array) - len(tsp_dises):], tsp_dises, label="Fixed route tsp distance")
+    plt.xlabel('Num of demands')
     plt.ylabel('Distances')
     ax2.legend()
     ax2.set_title(f'{num_fixed_stops} with {discount_ratio} as discount stops \'s distances')
-    fig2.savefig(f'variant_demand_size/{num_fixed_stops}_stops', format='png')
+    fig2.savefig(f'variant_demand_size/{num_fixed_stops}_stops.png')
